f5cb27da09edd7d3b9ff072bafb49c8265920bfa
hazemali1 <hazemali1343@gmail.com>
2023-08-01 16:41:41 -0700

    phone number

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+d = /^\d{10}/
+s = ARGV[0]
+if s.match?(d)
+  w = s.scan(d)
+  q = w.length
+  while q > 0 do
+    a = s.scan(d)
+    print a.join('')
+    q -= 1
+  end
+end
+puts ""
