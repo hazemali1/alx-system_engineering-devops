84a9c7c048a3ca22a892cd1d07ea51054ed63dc1
hazemali1 <hazemali1343@gmail.com>
2023-08-01 17:47:23 -0700

    text me

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/env ruby
+d = /from:(.*?)\]/
+x = /to:(.*?)\]/
+z = /flags:(.*?)\]/
+s = ARGV[0]
+print s.scan(d).join('')
+print ","
+print s.scan(x).join('')
+print ","
+print s.scan(z).join('')
+puts ""
