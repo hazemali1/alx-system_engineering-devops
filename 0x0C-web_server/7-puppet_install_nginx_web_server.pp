6f34138c3001ee79b43e743b90445801f9e3ba41
hazemali1 <hazemali1343@gmail.com>
2023-09-26 17:44:30 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,8 +1,8 @@
 # install nginx
 
 Exec { 'update',
-  command => 'sudo apt update',
-  provider => 'shell',
+  command  => 'sudo apt update',
+  provider => shell,
 }
 
 Package { 'nginx':
@@ -11,20 +11,23 @@ Package { 'nginx':
 
 Exec { 'hello_world':
   command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
 }
 
 Exec { 'redirect_me':
   command => 'echo "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root   /etc/nginx/html;
-    index  index.html index.htm;
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}" > /etc/nginx/sites-available/default',
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      root   /etc/nginx/html;
+      index  index.html index.htm;
+      location /redirect_me {
+          return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+      }
+  }" > /etc/nginx/sites-available/default',
+  provider => shell,
 }
 
 Exec { 'restart':
   command => 'sudo service nginx restart',
+  provider => shell,
 }
