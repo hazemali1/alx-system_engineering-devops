fb758d5bda61ee1514db7626ee29181cedf0b2e3
hazemali1 <hazemali1343@gmail.com>
2023-09-26 18:18:12 -0700

    puppsettt

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,14 +16,14 @@ Exec { 'hello_world':
 
 Exec { 'redirect_me':
   command  => 'echo "server {
-                  listen 80 default_server;
-                  listen [::]:80 default_server;
-                  root   /etc/nginx/html;
-                  index  index.html index.htm;
-                  location /redirect_me {
-                      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-                  }
-              }" > /etc/nginx/sites-available/default',
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root   /etc/nginx/html;
+    index  index.html index.htm;
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}" > /etc/nginx/sites-available/default',
   provider => shell,
 }
 
