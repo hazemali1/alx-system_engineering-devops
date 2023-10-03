891e2653d2e092118029376224424174dbc30260
hazemali1 <hazemali1343@gmail.com>
2023-10-02 17:43:56 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,4 @@
-# This puppet manifesto automates the task of creating a custom HTTP header
+# custom http
 
 exec { 'update':
   command => '/usr/bin/apt-get -y update',
@@ -9,28 +9,12 @@ package { 'nginx':
   require => Exec['update']
 }
 
-file_line { 'redirect':
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
-  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
+Exec { 'add_header':
+  command  => 'sudo sed -i "/http {/a \\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
+  provider => shell,
 }
 
-file_line { 'header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'add_header X-Served-By "$HOSTNAME";',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
-  require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+Exec { 'restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
