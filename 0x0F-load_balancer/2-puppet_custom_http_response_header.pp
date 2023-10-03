ab0582bc659e12127efd084d67a56ed6b9b814ec
hazemali1 <hazemali1343@gmail.com>
2023-10-02 17:51:32 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,14 +9,6 @@ package { 'nginx':
   require => Exec['update']
 }
 
-file_line { 'redirect':
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
-  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
 file_line { 'header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
