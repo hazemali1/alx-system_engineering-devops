d8e8465303aa436bbeee09c82a5a5c3aad9efe4d
hazemali1 <hazemali1343@gmail.com>
2023-10-02 17:40:05 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,11 +5,21 @@ Exec { 'update':
   provider => shell,
 }
 
-file_line { 'header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'add_header X-Served-By "$HOSTNAME";',
+file_line { 'redirect':
+  ensure   => 'present',
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+Exec { 'add_header':
+  command  => 'sudo sed -i "/http {/a \\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
+  provider => shell,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
   require => Package['nginx'],
 }
 
