de3af057f0758a41f3e420697db10ec01a2633a6
hazemali1 <hazemali1343@gmail.com>
2023-10-02 17:18:38 -0700

    puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+# custom http
+
+Exec { 'update':
+  command  => 'sudo apt update ; sudo apt install -y nginx',
+  provider => shell,
+}
+
+Exec { 'add_header':
+  command  => 'sudo sed -i '/http {/a \\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf',
+  provider => shell,
+}
+
+Exec { 'restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
