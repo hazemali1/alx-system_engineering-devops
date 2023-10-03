81f53a970c35e9c981e65e922e79a3a10c15af1e
hazemali1 <hazemali1343@gmail.com>
2023-10-02 18:29:41 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,6 +5,11 @@ exec { 'update':
   provider => shell,
 }
 
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update']
+}
+
 exec { 'add_header':
   command  => 'sudo sed -i "/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
   provider => shell,
