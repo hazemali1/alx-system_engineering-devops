827acffa743b5035fdaf40a6d5ed694cd427e879
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-24 00:47:39 +0300

    get info

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,6 +5,5 @@ import sys
 
 user = requests.get('https://jsonplaceholder.typicode.com/users').json()
 for u in user:
-	for k , v in u.items():
-		if k == 'id' and v == 2:
-			print(dict(k))
+	if u.keys().id == 2:
+		print("Please")
