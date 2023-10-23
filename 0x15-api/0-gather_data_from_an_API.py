cc2d8fc1c64e53595542fa20ccbf765bbdf88aed
hazemali1 <hazemali1343@gmail.com>
2023-10-23 15:20:20 -0700

    pycode

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,23 +9,23 @@ request and sys
 """
 
 if __name__ == "__main__":
-user = requests.get('https://jsonplaceholder.typicode.com/users').json()
-todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-for u in user:
-	if u['id'] == int(sys.argv[1]):
-			name = u['name']
+    user = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    for u in user:
+        if u['id'] == int(sys.argv[1]):
+            name = u['name']
 
-numbers = 0
-done = 0
-for t in todo:
-	if t['userId'] == int(sys.argv[1]):
-			numbers += 1
-			if t['completed'] is True:
-					done += 1
+    numbers = 0
+    done = 0
+    for t in todo:
+        if t['userId'] == int(sys.argv[1]):
+            numbers += 1
+            if t['completed'] is True:
+                done += 1
 
-print("Employee {} is done with tasks({}/{}):".format(name, done, numbers))
+    print("Employee {} is done with tasks({}/{}):".format(name, done, numbers))
 
-for t in todo:
-	if t['userId'] == int(sys.argv[1]):
-			if t['completed'] is True:
-					print("\t " + t['title'])
+    for t in todo:
+        if t['userId'] == int(sys.argv[1]):
+            if t['completed'] is True:
+                print("\t " + t['title'])
