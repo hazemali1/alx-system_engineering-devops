cc828d1a3472c535cfc375c197820fe427b4c92e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-24 01:00:13 +0300

    get info

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,6 +4,17 @@ import sys
 
 
 user = requests.get('https://jsonplaceholder.typicode.com/users').json()
+todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 for u in user:
 	if u['id'] == int(sys.argv[1]):
-		print(u['name'])
+		name = u['name']
+
+numbers = 0
+done = 0
+for t in todos:
+	if t['userId'] == int(sys.argv[1]):
+		numbers += 1
+		if t['completed'] == 'true':
+			done += 1
+
+print("Employee {} is done with tasks({}/{}}):".format(name, numbers, done))
