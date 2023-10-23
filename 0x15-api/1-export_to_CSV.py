c897c0a457a4ee1427210b1367a0a548841ac509
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-24 01:55:03 +0300

    get info

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,20 +12,21 @@ request and sys
 if __name__ == "__main__":
     user = requests.get('https://jsonplaceholder.typicode.com/users').json()
     todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-    with open(sys.argv[1] + ".csv", "w") as f:
-        w = csv.writer(f)
+
     for u in user:
         if u['id'] == int(sys.argv[1]):
             name = u['username']
 
-    for t in todo:
-        lis = []
-        if t['userId'] == int(sys.argv[1]):
-            lis.append(sys.argv[1])
-            lis.append(',')
-            lis.append(name)
-            lis.append(',')
-            lis.append(t['completed'])
-            lis.append(',')
-            lis.append(t['title'])
-            w.writerow(lis)
+    with open(sys.argv[1] + ".csv", "w") as f:
+        w = csv.writer(f)
+        for t in todo:
+            lis = []
+            if t['userId'] == int(sys.argv[1]):
+                lis.append(sys.argv[1])
+                lis.append(',')
+                lis.append(name)
+                lis.append(',')
+                lis.append(t['completed'])
+                lis.append(',')
+                lis.append(t['title'])
+                w.writerow(lis)
