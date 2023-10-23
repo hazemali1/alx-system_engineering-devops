43561b94b0158dabeab986b0388bc4fd04df5c35
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-24 02:16:03 +0300

    get info

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+decumented for models
+"""
+import json
+import requests
+import sys
+"""
+request and sys
+"""
+
+if __name__ == "__main__":
+    user = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    for u in user:
+        if u['id'] == int(sys.argv[1]):
+            name = u['username']
+
+    lis = []
+    my_dict = {}
+    for t in todo:
+        dic = {}
+        if t['userId'] == int(sys.argv[1]):
+            dic["task"] = t['title']
+            dic["completed"] = t['completed']
+            dic["username"] = name
+            lis.append(dic)
+
+    my_dict[sys.argv[1]] = lis
+    json_obj = json.dumps(my_dict)
+
+    with open(sys.argv[1] + ".json", "w") as f:
+        f.write(my_dict)
