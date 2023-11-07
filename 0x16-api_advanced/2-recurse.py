a658d2e82f48837a822563ad8d072243f19f73e9
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-07 19:51:55 +0200

    top ten

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+import requests
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    subscribers
+    """
+    req = requests.get("https://www.reddit.com/r/{}/hot.json"
+                       .format(subreddit),
+                       headers={"User-Agent": "User-Agent"})
+    if req.status_code < 300:
+        for s in range(0, len(req.json().get("data").get("children")[s])):
+            hot_list.append(req.json().get("data").get("children")[s]
+                     .get("data").get("title"))
+    else:
+        return None
