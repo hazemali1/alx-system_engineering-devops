e777c5e7dd19c838078289d67ea2130182a743fb
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-07 18:26:28 +0200

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,6 +13,6 @@ def top_ten(subreddit):
                        .format(subreddit),
                        headers={"User-Agent": "User-Agent"})
     if req.status_code < 300:
-        print(req.json().get("data").get("children").get("data"))
+        print(req.json().get("data").json().get("children").json().get("data").json().get("title"))
     else:
         print(None)
