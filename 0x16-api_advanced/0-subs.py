65ca6f46066bb29233128d4b3836aab6c750bc5a
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-07 17:04:05 +0200

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     """
     req = requests.get("https://www.reddit.com/r/{}/about.json"
                        .format(subreddit))
-    if req.status_code < 300:
+    if req.status_code != 404:
         return (req.json().get("data").get("subscribers"))
     else:
         return 0
