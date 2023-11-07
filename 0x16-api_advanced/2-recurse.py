c0bf67450543395e759702197f635cf593966a09
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-07 20:20:45 +0200

    top ten

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,8 +13,9 @@ def recurse(subreddit, hot_list=[], s=0):
                        .format(subreddit),
                        headers={"User-Agent": "User-Agent"})
     if req.status_code < 300:
-        hot_list.append(req.json().get("data").get("children")[s]
-                           .get("data").get("title"))
+        if req.json().get("data").get("children")[s]:
+            hot_list.append(req.json().get("data").get("children")[s]
+                               .get("data").get("title"))
         if req.json().get("data").get("after"):
             print("hello")
             return recurse(subreddit, hot_list, s + 1)
