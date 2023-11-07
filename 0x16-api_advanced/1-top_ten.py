ec777fa63b4a45868ebcf9e7be68b20042410571
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-07 18:20:38 +0200

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+import requests
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    subscribers
+    """
+    req = requests.get("https://www.reddit.com/r/{}/hot.json"
+                       .format(subreddit),
+                       headers={"User-Agent": "User-Agent"})
+    if req.status_code < 300:
+        print(req.get("data").get("children").get("data").get("title"))
+    else:
+        print(None)
