764e488c127f78b33fdc9012fcb591bfe020cf1f
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-17 03:58:00 +0200

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # http request
 exec { 'sed':
   command  => 'sudo sed -i s/15/4096/ /etc/default/nginx;
-	sudo service nginx restart',
-	provider => shell
+  sudo service nginx restart',
+  provider => shell
 }
